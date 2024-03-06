094dcfa87ca40d55a811cda8057ad531c26f0222
dashingaryan07 <114604574+dashingaryan07@users.noreply.github.com>
2024-03-07 01:56:31 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -128,7 +128,7 @@ const App = () => {
     const username = result.value;
     setPlayerName(username);
 
-    const newSocket = io("http://localhost:3000", {
+    const newSocket = io("https://tic-tac-toe-backend-m3tr.onrender.com", {
       autoConnect: true,
     });
 
